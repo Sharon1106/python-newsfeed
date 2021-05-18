049d9048a42e5e7d43b579a065641116937689f9
sharon <martinezsharonr@gmail.com>
2021-05-18 13:44:10 -0800

    Set up app to render templates-#2/front-end

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+# imports home bp
+from app.routes import home, dashboard
+# needed to run flask
+from flask import Flask
+
+def create_app(test_config=None):
+  # set up app config
+  app = Flask(__name__, static_url_path='/')
+  app.url_map.strict_slashes = False
+  app.config.from_mapping(
+    SECRET_KEY='super_secret_key'
+  )
+
+  @app.route('/hello')
+  def hello():
+    return 'Hello world!'
+
+  # register routes
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
+  
+  return app
