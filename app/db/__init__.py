33d53652c96b6466943503146c477f4983ba5a0d
sharon <martinezsharonr@gmail.com>
2021-11-10 22:08:26 -0800

    homepage views

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,12 +3,34 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
 # connect to database using env variable
-engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
-# generates temp connection for CRUD operations 
+engine = create_engine(getenv("DB_URL"), echo=True, pool_size=20, max_overflow=0)
+# generates temp connection for CRUD operations
 Session = sessionmaker(bind=engine)
 # base class variable helps map the models to real MySql tables
 Base = declarative_base()
+
+
+def init_db(app):
+    Base.metadata.create_all(engine)
+
+    app.teardown_appcontext(close_db)
+
+
+def get_db():
+    if "db" not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
